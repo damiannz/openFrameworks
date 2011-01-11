3eec739a7950599d034c41d0a378f56a8640ef4d
damian <damian@frey.NOSPAMco.nz>
2011-01-11 12:12:58 -0500

    avoid a hang on ofSoundStreamClose
    	modified:   sound/ofSoundStream.cpp

--- libs/openFrameworks/sound/ofSoundStream.cpp
+++ libs/openFrameworks/sound/ofSoundStream.cpp
@@ -147,7 +147,7 @@ void ofSoundStreamStart(){
 void ofSoundStreamClose(){
 	if(!audio) return;
 	try {
-    	audio->stopStream();
+    	//audio->stopStream();
     	audio->closeStream();
   	} catch (RtError &error) {
    		error.printMessage();
