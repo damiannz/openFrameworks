3d9dba90595ca77b2b416a6eefc633116cffb29f
damian <damian@frey.NOSPAMco.nz>
2012-09-18 11:58:28 +0200

    added ofEllipse prototype to ofGraphics.h

--- libs/openFrameworks/graphics/ofGraphics.h
+++ libs/openFrameworks/graphics/ofGraphics.h
@@ -182,6 +182,8 @@ void ofRectRounded(const ofPoint & p,float w,float h,float r);
 void ofRectRounded(float x,float y,float w,float h,float r);
 void ofRectRounded(float x,float y,float z,float w,float h,float r);
 
+void ofEllipse( float x, float y, float width, float height );
+
 void ofCurve(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3);
 void ofCurve(float x0, float y0, float z0, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
 void ofBezier(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3);
